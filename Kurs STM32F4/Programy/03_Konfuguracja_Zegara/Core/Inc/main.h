6f70d5286e84fa5e72885ac3cc6ec55d86356947
MrManus <40211714+MrManus@users.noreply.github.com>
2021-03-16 18:54:30 +0100

    STM32F4 #6 Liczniki, Konfiguracja zegara, Dubugger

--- Kurs STM32F4/Programy/03_Konfuguracja_Zegara/Core/Inc/main.h
+++ Kurs STM32F4/Programy/03_Konfuguracja_Zegara/Core/Inc/main.h
@@ -58,6 +58,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LED_BLUE_Pin GPIO_PIN_15
+#define LED_BLUE_GPIO_Port GPIOD
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
