01fdf5d5958790ed5acda2fe9e2ed63913c181ac
MrManus <40211714+MrManus@users.noreply.github.com>
2021-11-11 17:18:18 +0100

    led matrix working ok

--- Tuner/ManusTunerSTM64F411RE/Core/Src/main.c
+++ Tuner/ManusTunerSTM64F411RE/Core/Src/main.c
@@ -46,23 +46,52 @@ typedef union
 	uint16_t raw;
 }display_frm_u;
 
+#define MAX7219_NOOP_REGISTER   0x00
+#define MAX7219_DIGIT0_REGISTER 0x01
+#define MAX7219_DIGIT1_REGISTER 0x02
+#define MAX7219_DIGIT2_REGISTER 0x03
+#define MAX7219_DIGIT3_REGISTER 0x04
+#define MAX7219_DIGIT4_REGISTER 0x05
+#define MAX7219_DIGIT5_REGISTER 0x06
+#define MAX7219_DIGIT6_REGISTER 0x07
+#define MAX7219_DIGIT7_REGISTER 0x08
+#define MAX7219_DECODE_MODE_REGISTER  0x09
+#define MAX7219_INTENSITY_REGISTER   0x0A
+#define MAX7219_SCAN_LIMIT_REGISTER   0x0B
+#define MAX7219_SHUTDOWN_REGISTER    0x0C
+#define MAX7219_DISPLAY_TEST_REGISTER 0x0F
+
 typedef enum
 {
-	NO_OP = 0x00u,
-	DIGIT_0 = 0x01u,
-	DIGIT_1 = 0x02u,
-	DIGIT_2 = 0x03u,
-	DIGIT_3 = 0x04u,
-	DIGIT_4 = 0x05u,
-	DIGIT_5 = 0x06u,
-	DIGIT_6 = 0x07u,
-	DIGIT_7 = 0x08u,
-	DECODE_MODE = 0x09u,
-	INTESITY = 0x0Au,
-	SCAN_LINIT = 0x0Bu,
-	SHUTDOWN = 0x0Cu,
-	DISPLAY_TEST = 0x0Fu
-}display_cmd_e;
+	NoDecode 		= 0,
+	DecodeFor0		= 1,
+	DecodeFor3_0	= 2,
+	DecodeFor7_0	= 3
+}MAX7219_DecodeMode;
+
+typedef enum
+{
+	SleepMode 		= 0,
+	NormalOperation	= 1
+}MAX7219_ShutdownMode;
+
+typedef enum
+{
+	TestOff		= 0,
+	TestMode	= 1
+}MAX7219_TestMode;
+
+typedef enum
+{
+	ScanDigit0		= 0,
+	ScanDigit0_1	= 1,
+	ScanDigit0_2	= 2,
+	ScanDigit0_3	= 3,
+	ScanDigit0_4	= 4,
+	ScanDigit0_5	= 5,
+	ScanDigit0_6	= 6,
+	ScanDigit0_7	= 7,
+}MAX7219_ScanLimit;
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
@@ -93,7 +122,7 @@ static void MX_NVIC_Init(void);
 /* USER CODE BEGIN PFP */
 void display_task(void);
 void adc_data_processing_task(void);
-void disp_send_cmd(display_cmd_e cmd, uint8_t val);
+void disp_send_cmd(uint8_t cmd, uint8_t val);
 void disp_init(void);
 /* USER CODE END PFP */
 
@@ -246,12 +275,13 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 		adc_data_processing_task();
 
 		display_task();
+
 	}
 }
 
 void display_task(void)
 {
-//	  disp_send_cmd(DIGIT_3, 0x0F);
+
 }
 
 void disp_init()
@@ -259,23 +289,43 @@ void disp_init()
   HAL_GPIO_WritePin(DISP_CS_GPIO_Port, DISP_CS_Pin, GPIO_PIN_SET);
 
   HAL_Delay(1);
-  /* Normal Operation mode*/
-  disp_send_cmd(SHUTDOWN, 0x01);
+  /*Dummy byte*/
+  disp_send_cmd(MAX7219_NOOP_REGISTER, 0x00);
   HAL_Delay(1);
-
-  HAL_Delay(1);
-  /* Normal Operation mode*/
-  disp_send_cmd(SHUTDOWN, 0x01);
+  /* Decode mode: no decode */
+  disp_send_cmd(MAX7219_DECODE_MODE_REGISTER, NoDecode);
   HAL_Delay(1);
   /* Set Intensity */
-  disp_send_cmd(INTESITY, 0x0F);
+  disp_send_cmd(MAX7219_INTENSITY_REGISTER, 1);
   HAL_Delay(1);
   /* Set scan Limit */
-  disp_send_cmd(SCAN_LINIT, 0x07);
+  disp_send_cmd(MAX7219_SCAN_LIMIT_REGISTER, ScanDigit0_7);
+  HAL_Delay(1);
+  /* Turn off test mode */
+  disp_send_cmd(MAX7219_DISPLAY_TEST_REGISTER, TestOff);
+  HAL_Delay(1);
+  /* Normal Operation mode*/
+  disp_send_cmd(MAX7219_SHUTDOWN_REGISTER, NormalOperation);
+  HAL_Delay(1);
+  disp_send_cmd(MAX7219_DIGIT0_REGISTER, 0b00000001);
+  HAL_Delay(1);
+  disp_send_cmd(MAX7219_DIGIT1_REGISTER, 0b00000010);
+  HAL_Delay(1);
+  disp_send_cmd(MAX7219_DIGIT2_REGISTER, 0b00000100);
+  HAL_Delay(1);
+  disp_send_cmd(MAX7219_DIGIT3_REGISTER, 0b00001000);
+  HAL_Delay(1);
+  disp_send_cmd(MAX7219_DIGIT4_REGISTER, 0b00010000);
+  HAL_Delay(1);
+  disp_send_cmd(MAX7219_DIGIT5_REGISTER, 0b00100000);
+  HAL_Delay(1);
+  disp_send_cmd(MAX7219_DIGIT6_REGISTER, 0b01000000);
+  HAL_Delay(1);
+  disp_send_cmd(MAX7219_DIGIT7_REGISTER, 0b10000000);
   HAL_Delay(1);
 }
 
-void disp_send_cmd(display_cmd_e cmd, uint8_t val)
+void disp_send_cmd(uint8_t cmd, uint8_t val)
 {
 	disp_frm_buf.addr = cmd;
 	disp_frm_buf.val = val;
